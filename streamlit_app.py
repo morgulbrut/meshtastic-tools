47cadf5dd4edde97440e7cabfcc898efba3798de
morgulbrut <tillo.bosshart@gmail.com>
2025-01-20 13:18:21 +0100

    Initial & gitignore

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,31 @@
 import streamlit as st
+import folium
+from streamlit_folium import st_folium
+import rangetest
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.set_page_config(
+    page_title='Meshtatic Tools',
+    page_icon=':rocket:',  # This is an emoji shortcode. Could be a URL too.
+    layout='wide'
 )
+
+# pg = st.navigation([st.Page("map.py"), st.Page("hello.py")])
+
+# pg.run()
+
+# tab1, tab2, tab3, tab4, tab5, tab6, tab7, tab8 = st.tabs(
+#     ["Rangetest Visualizer", "Tab 2", "Tab 3", "Tab 4", "Tab 5", "Tab 6", "Tab 7", "Tab 8"]
+# )
+
+st.title("Meshtatic Tools")
+
+tab1, tab2 = st.tabs(
+    ["Readme", "Rangetest Visualizer"]
+)
+
+with tab1:
+    with open("README.md", "r") as f:
+        st.markdown(f.read())
+
+with tab2:
+    rangetest.show()
