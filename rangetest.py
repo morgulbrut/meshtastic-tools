27758fc69fed47db244aca2cac4631ae86420571
morgulbrut <tillo.bosshart@gmail.com>
2025-01-20 13:17:44 +0100

    Rangetest added

---
+++ rangetest.py
@@ -0,0 +1,72 @@
+import streamlit as st
+import folium
+from streamlit_folium import st_folium
+import pandas as pd
+
+colors = ['purple', 'green', 'darkred', 'blue', 'red']
+
+
+@st.cache_data
+def get_data(file):
+    df = pd.read_csv(file)
+    df = df[df['rx lat'].notna()]
+    df = df[df['sender lat'].notna()]
+    df = df[df['sender name'].notna()]
+    return df
+
+
+def show():
+    file = st.file_uploader('Upload a rangetest file', type=['csv'])
+
+    if file:
+        data = get_data(file)
+
+        if data is not None:
+
+            st.sidebar.subheader('Rangetest Data')
+
+            min_distance = st.sidebar.slider(
+                'Minimum distance (km)', 0, 150, 0, 1)
+            data = data[data['distance'] > min_distance * 1000]
+
+            sender_names = sorted(data['sender name'].unique())
+            selected_senders = st.sidebar.multiselect(
+                'Select senders', sender_names)
+            if selected_senders:
+                data = data[data['sender name'].isin(selected_senders)]
+
+            hop_limit = sorted(data['hop limit'].unique())
+            selected_hop_limit = st.sidebar.multiselect(
+                'Select hop limit', hop_limit)
+            if selected_hop_limit:
+                data = data[data['hop limit'].isin(selected_hop_limit)]
+
+            m = folium.Map(location=[data['sender lat'].mean(
+            ), data['sender long'].mean()], zoom_start=10)
+
+            drawn_sender = []
+            col_indx = 0
+
+            for i, row in data.iterrows():
+                if row['sender name'] not in drawn_sender:
+                    drawn_sender.append(row['sender name'])
+                    sender = row['sender name']
+                    folium.Marker(
+                        [row['sender lat'], row['sender long']], popup=row['sender name'], tooltip=row['sender name'], icon=folium.Icon(
+                            color=colors[col_indx])
+                    ).add_to(m)
+
+                    if len(selected_senders) > 0 or min_distance > 10:
+                        for i, row in data[data['sender name'] == sender].iterrows():
+                            folium.PolyLine(
+                                locations=[(row['sender lat'], row['sender long']),
+                                           (row['rx lat'], row['rx long'])],
+                                color=colors[col_indx],
+                                weight=10,
+                                opacity=0.2,
+                                tooltip=f"{row['distance']/1000} km"
+                            ).add_to(m)
+                    col_indx = (col_indx + 1) % len(colors)
+
+            # call to render Folium map in Streamlit
+            st_data = st_folium(m, width=1980, height=1080)
